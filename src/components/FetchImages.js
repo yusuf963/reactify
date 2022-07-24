ef26e1cbc24eb9cac5984b5dadc20449dd471222
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-24 16:46:36 +0100

    handle child passing props to parent

--- src/components/FetchImages.js
+++ src/components/FetchImages.js
@@ -9,15 +9,14 @@ const call_config = {
 class FetchImages extends React.Component {
   state = {
     term: null,
-    data: null
+    images: []
   }
 
   // waht is this doing
   handleOnSearchSubmit(term) {
-    console.log(term)
   }
 
-  async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
     const response = await axios.get(call_config.url, {
       params: {
         query: term
@@ -25,19 +24,35 @@ class FetchImages extends React.Component {
       headers: {
         Authorization: `Client-ID ${call_config.acess_key}`
       }
-    }).then((res) => {
-      console.log(res.data.results)
-      this.setState({ data: res.data })
     })
+    this.setState({ images: response.data.results })
+
+
+
+    // fetch(call_config.url,
+    //   {
+    //     params: {
+    //       query: term
+    //     },
+    //     headers: {
+    //       Authorization: `Client-ID ${call_config.acess_key}`
+    //     }
+    //   }
+    // ).then((res) => {
+    //   console.log(res)
+    // })
   }
 
   render() {
     // const handleTermInput = (e) => {
     //   this.setState({ term: e.target.value })
     // }
-    console.log(this.state.data)
     return (
-      <SearchBar onSubmit={this.onSearchSubmit} />
+      <div>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <p>Found {this.state.images.length}</p>
+
+      </div>
     );
   }
 }
