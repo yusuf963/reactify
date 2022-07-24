e237a1d880192823595b5f460ada50f579ae0211
yusuf963 <mohammad963yusuf@gmail.com>
2022-07-24 18:55:39 +0100

    create imahelist component and css

--- src/components/fetchApp/ImageList.js
+++ src/components/fetchApp/ImageList.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-
+import './imageList.css'
 const ImageList = (props) => {
-  const images = props.images.map((image, index) => {
-    return (
-      <img
-        key={index}
-        src={image.urls.small}
-        alt="any"
-      />
-    )
+  const images = props.images.map(({ urls, id, description }) => {
+    return <img
+      key={id}
+      src={urls.small}
+      alt={description}
+    />
   })
-  return (
-    <div>
-      {images}
-
-    </div>
-  )
+  return <div className="image-list">{images}</div>
 }
 
 export default ImageList;
